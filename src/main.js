7e1daa2c5c81a81e5e481cb9fb8d5425c183f21d
Ruslan Shamsutdinov <Ruslan_Shamsutdinov@epam.com>
2022-01-10 18:28:11 +0300

    fix SecretGenerator.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const consoleInput = require("readline-sync");
 const UserInput = require("./class/UserInput")
 const ConsoleWrite = require("./class/ConsoleWrite");
+const Secret = require("./class/Secret");
 const SecretGenerator = require("./class/SecretGenerator");
 const Guess = require("./class/Guess");
 const GuessTheNumberLogic = require("./class/GuessTheNumberLogic");
@@ -8,7 +9,7 @@ const Game = require("./class/Game");
 
 const userInput = new UserInput(consoleInput);
 const userOutput = new ConsoleWrite();
-const secretGenerator = new SecretGenerator();
+const secretGenerator = new SecretGenerator(Secret);
 const guess = new Guess(userInput, userOutput);
 const guessTheNumberLogic = new GuessTheNumberLogic(secretGenerator, guess, 5);
 
